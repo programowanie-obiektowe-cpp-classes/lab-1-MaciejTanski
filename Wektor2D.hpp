2d994f9bd2c264bb57bf69b02e2635c533f49419
MaciejTanski <tanior0803@gmail.com>
2025-10-18 13:01:04 +0200

    All test passed

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,38 @@
+#include <cmath>
+#include <print>
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+  public:
+
+        Wektor2D(double x, double y){
+        x_ = x; y_=y;  
+      }
+
+      Wektor2D() { 
+        x_ = 0.; y_ = 0.;
+      }
+
+      void setX(double x) { x_ = x; }
+      void setY(double y) { y_ = y; }
+
+      double getX() const { return x_; }
+      double getY() const { return y_; }  
+
+  private:
+      double x_,y_;
+
 };
+
+
+Wektor2D operator+(const Wektor2D& w1, const Wektor2D& w2)
+{
+    return Wektor2D{w1.getX() + w2.getX(), w1.getY() + w2.getY()};
+}
+
+double operator*(const Wektor2D& w1, const Wektor2D& w2)
+{
+    return w1.getX() * w2.getX() + w1.getY() * w2.getY();
+}
+
+
